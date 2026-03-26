0a44c526f1e2738ee20526722d6075dda45932eb
erroop <erroop0@gmail.com>
2026-03-26 18:10:49 +0900

    서버 업데이트

--- server.js
+++ server.js
@@ -4,18 +4,13 @@ import jsonServer from "json-server";
 import cors from "cors";
 
 const app = express();
-
-// json-server
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 app.use(cors());
-app.use(middlewares); // 🔥 이거 추가
 
-// API
 app.use("/api", router);
 
-// 정적 파일
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "dist")));
 
@@ -23,8 +18,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// 🔥 핵심: host 추가
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`server running on ${PORT}`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
